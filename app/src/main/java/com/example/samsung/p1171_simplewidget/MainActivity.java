b735efd88dd7d301436b547f6dfedf3af58e6700
btow <btow@yandex.ru>
2017-05-12 16:39:45 +0500

    Initial commit. And finish

---
+++ app/src/main/java/com/example/samsung/p1171_simplewidget/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.samsung.p1171_simplewidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
